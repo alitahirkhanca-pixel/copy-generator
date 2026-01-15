d62f33594c62354a8beaa8dce382efad21f247a4
Ali T Khan <alitahirkhan@MacBook-Air.local>
2026-01-14 23:42:45 -0600

    Add replit.nix for Python 3.11 and dependencies

---
+++ replit.nix
@@ -0,0 +1,10 @@
+{ pkgs }: {
+  deps = [
+    pkgs.python311
+    pkgs.python311Packages.flask
+    pkgs.python311Packages.flask-cors
+    pkgs.python311Packages.requests
+    pkgs.python311Packages.beautifulsoup4
+    pkgs.python311Packages.python-dotenv
+  ];
+}
